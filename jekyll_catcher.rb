eb6ba2893d548597f3df898b423d8d72a8de1ed1
Hannes Fostie <hannes@openminds.be>
2013-10-10 14:33:48 +0200

    Big refactor of the app

--- jekyll_catcher.rb
+++ jekyll_catcher.rb
@@ -3,37 +3,54 @@ require 'rack'
 require 'json'
 require 'yaml'
 
-class JekyllCatcher
-  def call(env)
-    @request = Rack::Request.new(env)
-    @response = Rack::Response.new
-    handle_request
-    @response.finish
-  end
-
-  def handle_request
-    config = YAML.load_file('config.yml')
-    @payload = JSON.parse(@request.POST["payload"])
-    repository = payload["repository"]["name"]
-
-    @application_config = config["applications"][repository]
-
-    @response.write "You're doing it wrong!" unless recognized_repository && originated_from_github
-    # `cd #{application["path"]} && git pull origin master && LC_ALL='en_US.UTF-8' bundle exec jekyll build`
-    JekyllCatcherTask.const_get(@application_config["jekyll_catcher_task_name"]).new(@application_config).call
-    @response.write "Well done, sir!"
-  end
-
-  def recognized_repository
-    repository_url = @payload["repository"]["url"]
-    @application_config["url"] == repository_url
-  end
-
-  def originated_from_github
-    [
-      IPAddr.new('192.30.252.0/22'),
-      IPAddr.new('204.232.175.64/27')
-    ].any? { |ip_range| ip_range.include?(@request.ip) }
-  end
+Dir[File.dirname(__FILE__) + 'tasks/*.rb'].each do |file|
+  require File.basename(file, File.extname(file))
 end
 
+module JekyllCatcher
+  class App
+    def call(env)
+      @request = Rack::Request.new(env)
+      @response = Rack::Response.new
+      handle_request
+      @response.finish
+    end
+
+    def handle_request
+      begin
+        throw(:invalid_request) unless originated_from_github?
+        JekyllCatcher::Task.const_get(camelize(@config['jekyll_catcher_task_name'])).new(@config).call
+      rescue
+        return @response.write "You're doing it wrong!"
+      end
+      @response.write "Well done, sir!"
+    end
+
+    def config
+      @config ||= parse_config
+    end
+
+    def parse_config
+      application_config = YAML.load_file('config.yml')[payload['name']]
+      throw(:application_not_in_config) unless application_config
+      throw(:repository_url_does_not_match) unless payload['url'] == application_config['url']
+      throw(:task_does_not_exist) unless defined?(camelize(application_config['jekyll_catcher_task_name']))
+      application_config
+    end
+
+    def camelize(name)
+      name.gsub(/(?:^|_)(.)/) { $1.upcase }
+    end
+
+    def payload
+      @payload ||= JSON.parse(@request.POST['payload'])['repository']
+    end
+
+    def originated_from_github?
+      [
+        IPAddr.new('192.30.252.0/22'),
+        IPAddr.new('204.232.175.64/27')
+      ].any? { |ip_range| ip_range.include?(@request.ip) }
+    end
+  end
+end
