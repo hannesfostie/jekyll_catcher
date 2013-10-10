459a2a0b387427bdbc7e46a98a54713cfd2bf913
Hannes Fostie <hannes@openminds.be>
2013-10-10 19:57:19 +0200

    Improves local deploy task

--- tasks/local_deploy.rb
+++ tasks/local_deploy.rb
@@ -1,11 +1,15 @@
-module JekyllCatcherTask
-  class LocalDeploy
-    def initialize(config)
-      @config = config
-    end
+module JekyllCatcher
+  module Task
+    class LocalDeploy
+      def initialize(config)
+        @config = config
+      end
 
-    def call
-      puts @config["something_random"]
+      def call
+        run "cd #{@config['path']} && LC_ALL='en_US.UTF-8' bundle exec sass --update _sass:css -f"
+        run "cd #{@config['path']} && LC_ALL='en_US.UTF-8' bundle exec jekyll build --no-auto"
+      end
     end
   end
 end
+
